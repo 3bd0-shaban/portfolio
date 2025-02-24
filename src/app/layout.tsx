24596d0ab9d12c22b564e0a3e64e5745326f56a0
AbdElrahman Shaban <3bdoofficial0@gmail.com>
2025-02-24 17:04:15 +0200

    adding GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { siteConfig } from "@/config/siteConfig";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-H9BNH21TMH" />
       </body>
     </html>
   );
